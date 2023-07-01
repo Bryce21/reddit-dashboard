89906a108540bcf0bc984e353854f4cbdc7f5c6c
Bryce <bryce.reinhard@perkinelmer.com>
2023-07-01 14:37:12 -0400

    Add config file to avoid committing secrets

--- src/producer/praw/main.py
+++ src/producer/praw/main.py
@@ -2,32 +2,27 @@ import praw
 from classes import Post, Comment
 from kafka import ProducerWrapper
 import HealthCheck
-from confluent_kafka import Producer
-import socket
 import atexit
-from confluent_kafka import Producer
-import socket
-import asyncio
-import json
-
-
-
-reddit = praw.Reddit(
-    client_id="1lb14v6vpEASB83rdUeAGg",
-    client_secret="XFJourPG18yjLhwxMTeUnfMgWCYQFA",
-    user_agent="praw_reddit_producer"
-)
+import configparser
 
 
 if __name__ == '__main__':
+
+    config = configparser.ConfigParser()
+    config.read("./src/producer/praw/env.producer")
+
     producer = ProducerWrapper.ProducerWrapper()
     atexit.register(producer.close_producer)
 
-    health_check = HealthCheck.HealthCheck(producer, 5.0)
+    health_check = HealthCheck.HealthCheck(producer, float(str(config["PRAW_PRODUCER"]["HEALTHCHECK_INTERVAL"])))
     health_check.start()
 
 
-
+    reddit = praw.Reddit(
+        client_id=str(config["PRAW_PRODUCER"]["CLIENT_ID"]),
+        client_secret=str(config["PRAW_PRODUCER"]["CLIENT_SECRET"]),
+        user_agent=str(config["PRAW_PRODUCER"]["USER_AGENT"])
+    )
 
     subreddit = reddit.subreddit("all")
     for submission in subreddit.stream.submissions():
@@ -35,11 +30,6 @@ if __name__ == '__main__':
         sub = Post.Post.createFromPost(submission)
         producer.produce(topic="dirty_posts", key=sub.subredditName, value=sub.getJSON())
 
-        #print(sub.getJSON())
         for comment in submission.comments.list():
             c = Comment.Comment.createFromComment(comment, sub.id)
-            #print(c.getJSON())
             producer.produce("dirty_comments", key=c.postID, value=c.getJSON())
-            # print("Post id: " + sub.id + ", Author: " + c.author + ", Parent id: " + c.parent_id, "Subreddit: " + c.subredditName)
-
-
