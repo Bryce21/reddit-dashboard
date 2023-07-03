ebad34a1e2be1032679ee4377b899961ec837a34
Bryce <bryce.reinhard@perkinelmer.com>
2023-07-03 14:16:34 -0400

    Containerize kafka producer, fix env variables

--- src/producer/praw/main.py
+++ src/producer/praw/main.py
@@ -3,28 +3,32 @@ from classes import Post, Comment
 from kafka import ProducerWrapper
 import HealthCheck
 import atexit
-import configparser
+import os
+from dotenv import load_dotenv
 
 
 if __name__ == '__main__':
-
-    config = configparser.ConfigParser()
-    config.read("./src/producer/praw/env.producer")
-
-    producer = ProducerWrapper.ProducerWrapper()
+    load_dotenv()
+    producer = ProducerWrapper.ProducerWrapper(
+        host=os.getenv("KAFKA_HOST"),
+        port=os.getenv("KAFKA_PORT")
+    )
     atexit.register(producer.close_producer)
 
-    health_check = HealthCheck.HealthCheck(producer, float(str(config["PRAW_PRODUCER"]["HEALTHCHECK_INTERVAL"])))
+    health_check = HealthCheck.HealthCheck(
+        producer, float(str(os.getenv("HEALTHCHECK_INTERVAL")))
+    )
     health_check.start()
 
 
     reddit = praw.Reddit(
-        client_id=str(config["PRAW_PRODUCER"]["CLIENT_ID"]),
-        client_secret=str(config["PRAW_PRODUCER"]["CLIENT_SECRET"]),
-        user_agent=str(config["PRAW_PRODUCER"]["USER_AGENT"])
+        client_id=str(os.getenv("CLIENT_ID")),
+        client_secret=str(os.getenv("CLIENT_SECRET")),
+        user_agent=str(os.getenv("USER_AGENT"))
     )
 
     subreddit = reddit.subreddit("all")
+    print("Starting consumption")
     for submission in subreddit.stream.submissions():
         submission.comments.replace_more(limit=None)
         sub = Post.Post.createFromPost(submission)
